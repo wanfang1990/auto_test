2e9b0ac25f3d938dd117ec005d2f3082c037e499
wanfang1990 <1164403037@qq.com>
2019-04-25 18:49:43 +0800

    version1

---
+++ auto_test_webProject/src/test/java/auto_test_webProject/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package auto_test_webProject;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+	@Test
+	public void testCase1(){
+		System.out.println("******testCase1()********");
+	}
+
+}
